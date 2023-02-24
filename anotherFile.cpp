a073542198167f990de70136341b23e7225c78a4
kristel.trujillo <kristel.trujillo@M308-12>
2023-02-24 11:44:12 -0500

    Test

---
+++ anotherFile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "This is a test, only using cout" << endl;
+
+    int sum;
+    int a = 5;
+    int b = 5;
+    sum = a + b;
+
+    cout << "5 + 5 is: " << sum << endl;
+}
